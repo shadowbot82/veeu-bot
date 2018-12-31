2aec4a5906a0c5ec02f1dcb396aa29e7e65eeb49
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2018-12-31 21:21:22 +0700

    Update tahu2.php

--- tahu2.php
+++ tahu2.php
@@ -42,7 +42,7 @@ $coin=coin($token,$jumlah,$wait);
 echo "$turkis [$kuning+$turkis] $putih Penghasilan =>$ijo $cash";
 $cash=cash($token,$jumlah,$wait);
 	
-echo "$kuning ======================================\n";
+echo "$kuning ================================================\n";
  }
 
 function coin($token,$jumlah,$wait) {
