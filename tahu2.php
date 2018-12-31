cb6577dee5925a6d6ab6e1175de21d6ebb1b019b
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2018-12-31 19:00:23 +0700

    Update tahu2.php

--- tahu2.php
+++ tahu2.php
@@ -8,31 +8,66 @@ $red="\033[1;31m";
 $kuning="\033[1;33m";
 
 //flag
-print"$ijo
-========================================================\n
-[]  [] [][][] [][][]$red  !!!! WARNING !!!!$pink
-[]  [] []     []   []$red ==> Bot Ilegal <==$pink
-[][][] [][]   []   []$kuning bisa kena $red Banned$pink
-[]  [] []     []   []$pink pergunakan dengan bijak$ijo
-[]  [] [][][] [][][]$biru  Created$turkis By$ijo Haykal$pink Hernandez$kuning\n$ijo
-========================================================\n$biru";
-function id($token,$jumlah,$wait)
-{
-$x = 0;
-while($x < $jumlah) { $rand = rand(1,99999);
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://api.indkota.com//Content/Billing/billing?member_token='.$token.'&billing_id=$rand");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-$result = curl_exec($ch); curl_close ($ch);
-echo $result."\n"; sleep($wait); $x++;
-flush(); } } echo "Token ?\nInput : ";
+
+echo $ijo."[+]==========================================[+]\n";
+echo $turkis." _____  _  _   _ _   _  $red BOT TAHU VIP+\n";
+echo $pink."|__ __/  \| |_| | | | | $kuning Happy New Year 2019\n";
+echo $red."  | |/ _  \  _  | |_| | $ijo Created By : Shadow Bot\n";
+echo $biru."  |_|_/  \_\| |_|_____| $turkis YT : SHADOW BOT CHANNEL\n";
+echo $ijo."[+]==========================================[+]\n\n";
+    
+
+echo "Token ?\nInput : ";
 $token = trim(fgets(STDIN));
 echo "Jumlah ?\nInput : ";
 $jumlah = trim(fgets(STDIN));
 echo "Jeda ?(ex:0)\nInput : ";
 $wait = trim(fgets(STDIN));
-$submit = id($token,$jumlah,$wait);
 
+$i=0;
+while($i<$jumlah){
+  sleep($wait);
+  $i++;
+  flush();
+ }
+
+function coin($token,$jumlah,$wait) {
+
+	
+
+	$rand 	= rand(1,99999);
+    $ch 	= curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://api.indkota.com/Content/Content/read?member_token='.$token.'&content_id=$rand&lang=indonesian&device_id=$rand");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+    $result = curl_exec($ch);
+    curl_close ($ch);
+	echo $result."\n";
+    sleep($wait);
+    $x++;
+    flush();
+	} 
+
+
+function cash($token,$jumlah,$wait)
+{
+
+ $rand = rand(1,99999);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://api.indkota.com//Content/Billing/billing?member_token='.$token.'&billing_id=$rand");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+$result = curl_exec($ch); 
+curl_close ($ch);
+echo $result."\n"; 
+sleep($wait); 
+$x++;
+flush(); } 
+
+
+
+$submit = cash($token,$jumlah,$wait);
+$oce = coin($token,$jumlah,$wait);
 ?>
